7a03728e262236a1a9ced69d9427ed7caa5c9c95
MotiElitzur <mordehaiel@gmail.com>
2020-11-20 12:52:57 +0200

    - Add my java code - not working.

--- app/src/main/java/moti/bots/MainActivity.java
+++ app/src/main/java/moti/bots/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new ArticleHackerBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
